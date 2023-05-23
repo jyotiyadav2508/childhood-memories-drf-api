c34452242d81e10bb86890d57d14cf5fe23fa0d1
jyotiyadav2508 <>
2023-05-23 21:33:58 +0000

    Add LikeList and LikeDetail view

---
+++ likes/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
+
+
+class Like(models.Model):
+    """
+    Like model, related to 'owner' which is a User instance and
+    'post' which is a Post instance
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE,
+        default=None, null=True,
+        )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """
+        Orders Like objects in the order latest to old
+        'unique_together' makes sure a user can't like the same post twice
+        """
+        ordering = ['created_on']
+        unique_together = ['owner', 'post']
+
+    def __str__(self):
+        '''
+        Returns the string representation of a model instance
+        '''
+        return f'{self.owner} {self.post}'
---
+++ likes/serializers.py
@@ -0,0 +1,25 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from likes.models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'created_on', 'owner', 'post']
+
+    def create(self, validated_data):
+        '''
+        Handles duplicated likes by the same user
+        '''
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializer.ValidationError({
+        'detail': 'possible deplication'
+        })
