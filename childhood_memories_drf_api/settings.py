fefb837feb1bb9319bf3e3ea96af880813a07e6c
jyotiyadav2508 <>
2023-05-28 12:12:50 +0000

    Add client origin dev in settings.py

--- childhood_memories_drf_api/settings.py
+++ childhood_memories_drf_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 
 if os.path.exists('env.py'):
@@ -61,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'childhood-memories.herokuapp.com']
+ALLOWED_HOSTS = ['os.environ.get('ALLOWED_HOST')', 'localhost']
 
 
 # Application definition
@@ -109,9 +110,12 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^([^.]+)', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}.(eu|us)\d+\.codeanyapp\.com$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
